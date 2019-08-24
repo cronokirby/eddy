a5df73463eafefb4f87279f178a6730c6b828c5b
Lúcás Meier <cronokirby@gmail.com>
2019-08-24 18:42:31 -0400

    Add basic termion interaction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate termion;
+
+use std::io::{stdin, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = stdin();
+    let mut stdout = stdout()
+        .into_raw_mode()
+        .expect("Couldn't create raw mode terminal");
+    write!(stdout, "{}", termion::clear::All).unwrap();
+    for c in stdin.keys() {
+        write!(
+            stdout,
+            "{}{}",
+            termion::cursor::Goto(1, 1),
+            termion::clear::CurrentLine
+        )
+        .unwrap();
+        match c.unwrap() {
+            Key::Char('q') => break,
+            Key::Char(c) => println!("{}", c),
+            _ => {}
+        }
+    }
 }
